9643a60c5adfa8c1c49510ec0d5efc73f72529d3
ansonwcy <cywongci@connect.ust.hk>
2023-09-11 18:45:50 +0800

    change default data

--- src/data.json.ts
+++ src/data.json.ts
@@ -2,63 +2,22 @@ export default {
   "defaultBuilderData": {
     questions: [
       {
-        question: 'The transaction Merkle Tree root value in a Bitcoin block is calculated using,',
+        question: 'Sample Question',
         answers: [
           {
-            content: 'Hash of transactions',
+            content: 'Wrong Answer 1',
+            correct: false
+          },
+          {
+            content: 'Correct Answer',
             correct: true
           },
           {
-            content: 'Previous block\'s hash',
+            content: 'Wrong Answer 2',
             correct: false
           },
           {
-            content: 'Number of transactions',
-            correct: false
-          },
-          {
-            content: 'None of the above',
-            correct: false
-          }
-        ]
-      },
-      {
-        question: 'The height of the block is the ____ in the chain between it and the genesis block.',
-        answers: [
-          {
-            content: 'Metadata that is',
-            correct: false
-          },
-          {
-            content: 'Number of blocks',
-            correct: true
-          },
-          {
-            content: 'Merkle tree hash',
-            correct: false
-          },
-          {
-            content: 'Size of the memory cache',
-            correct: false
-          }
-        ]
-      }, {
-        question: 'What type of hash is used when there is a fixed number of items to be hashed, such as the items in a block header, and we are verifying the composite block integrity?',
-        answers: [
-          {
-            content: 'Tree-structured Hash',
-            correct: false
-          },
-          {
-            content: 'Complex hash',
-            correct: false
-          },
-          {
-            content: 'Simple Hash',
-            correct: true
-          },
-          {
-            content: 'Either',
+            content: 'Wrong Answer 3',
             correct: false
           }
         ]
